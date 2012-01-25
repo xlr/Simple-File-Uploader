94b31496410fab80b40e96248f4a35d229e8654d
GaD <for.forus@gmail.com>
2012-01-25 22:42:50 +0400

    Update file.php

--- file.php
+++ file.php
@@ -1,14 +1,8 @@
 <?php
 include('con.php');
-	if($_GET['id'])
-		{
-			if (isset($_GET['id']))
-			{$file = $_GET['id']; }
-if (!preg_match("|^[\d]+$|", $file))
-	{
-	exit ("Хацкир!!!1");
-	}
-		 $result = mysql_query("SELECT `id`,`nick`,`desc`,`count` FROM `hostf` where id = '$file'",$db);
+	if (isset($_GET['id']))	{$file = $_GET['id']; }
+	if (!preg_match("|^[\d]+$|", $file)){exit ("Хацкир!!!1");}
+		 $result = mysql_query("SELECT * FROM `hostf` where id = '$file'",$db);
 	if (mysql_num_rows($result) > 0)
 	{
 		$my = mysql_fetch_array($result);
@@ -26,6 +20,9 @@ if (!preg_match("|^[\d]+$|", $file))
 		</table>
 		";
 	}
-}
+		else
+	{
+		echo "Увы, такого файла нет!";
+	}
 
 ?>
